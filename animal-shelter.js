2075cd184ddc0344261fd21af1e28f6bbc3d2100
Lincoln Bunker <lincolnbunker03@gmail.com>
2024-02-12 14:17:46 -0700

    not till tmrw homie

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,19 @@
 const animalData = require('./animal-data.json');
+
+class Animal {
+    constructor (name, species, color, hunger = 50) {
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.hunger = hunger
+    }
+
+    greetMethod() {
+        console.log(`Hi, I'm ${this.name} the ${this.species}`) 
+    }
+
+    feedMethod() {
+        this.hunger -= 20
+        console.log("Yum, I love food")
+    }
+}
